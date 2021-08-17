0f065a936705b7338f122b805a1e705ba7444bf5
serendipity-harpoon <sunhaopeng2016@gmail.com>
2021-08-17 12:06:10 -0400

    use dispatch to edit the data in redux

---
+++ learn-redux/src/actions/index.js
@@ -0,0 +1,7 @@
+export const increment = () => {
+  return { type: 'INCREMENT'}
+}
+
+export const login = () => {
+  return { type: 'SIGN_IN' }
+}
--- learn-redux/src/compoments/Display.jsx
+++ learn-redux/src/compoments/Display.jsx
@@ -1,11 +1,19 @@
 import React from "react"
-import { useSelector } from "react-redux"
+import { useSelector, useDispatch } from "react-redux"
+import { increment, login } from "../actions"
 
 const Display = () => {
   const counter = useSelector(state => state.counter)
+  const islogged = useSelector(state => state.islogged)
+  const dispatch = useDispatch()
   return (
     <div>
       <h4>Counter {counter}</h4>
+      <div>
+        <button onClick={() => dispatch(increment())}>increment</button>
+      </div>
+      <button onClick={() => dispatch(login())}>log in</button>
+      {islogged && <h4>Valuable info should not be seen unless logged in</h4>}
     </div>
   )
 }
