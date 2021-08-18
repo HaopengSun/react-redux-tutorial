3364b104ecd0fcf787b41bde13190393607fdf9c
serendipity-harpoon <sunhaopeng2016@gmail.com>
2021-08-17 22:10:38 -0400

    fetch data using axios

--- reduxexample/src/components/Posts.jsx
+++ reduxexample/src/components/Posts.jsx
@@ -1,8 +1,30 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
+import axios from "axios"
 
 const Posts = function(){
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+      .then(function (response) {
+        setData(response.data)
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+  }, [])
+
+  const showData = function(arr){
+    return arr.map((info, idx) => {
+      return <div key={idx}>{info.title}</div>
+    })
+  }
+
   return (
-    <div>Posts</div>
+    <div>
+      <h3>Posts</h3>
+      { showData(data) }
+    </div>
   )
 }
 
